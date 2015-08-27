5f5ddce33c9f82cfe038365e917af7fea0b5c232
bauddhik <shoppingbauddhik@gmail.com>
2015-08-27 05:24:29 -0700

    Initial Commit

---
+++ MastarBlastar/src/main/java/com/mb/action/NewUserAction.java
@@ -0,0 +1,17 @@
+package com.mb.action;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class NewUserAction extends ActionSupport implements SessionAware{
+
+	@Override
+	public void setSession(Map<String, Object> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- MastarBlastar/src/main/webapp/index.jsp
+++ MastarBlastar/src/main/webapp/index.jsp
@@ -1,3 +1,6 @@
+ <%  
+     response.sendRedirect("form");  
+    %> 
 <html>
 <body>
 <h2>Hello World!</h2>
